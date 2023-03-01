6cf30dbf35bc5119c21079f7960fdf5b14a9c3e3
dimakrugly <106512918+dimakrugly@users.noreply.github.com>
2023-03-01 18:24:03 +0200

    feat: add language detector

--- src/services/translate/TranslateService.js
+++ src/services/translate/TranslateService.js
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import ua from './ua/ua';
 import en from './en/en';
 
@@ -17,12 +18,17 @@ class TranslateService {
 
   init() {
     i18n
+      .use(LanguageDetector)
       .use(initReactI18next) // passes i18n down to react-i18next
       .init({
         resources,
         fallbackLng: appLocales,
         interpolation: {
           escapeValue: false,
+        },
+        detection: {
+          order: ['cookie', 'localStorage'],
+          caches: ['cookie'],
         },
       });
   }
