3d7043ebc4d084832514c9c6227b1e7751f19091
dimakrugly <106512918+dimakrugly@users.noreply.github.com>
2023-03-12 09:56:30 +0200

    fix: fix console errors

--- src/pages/home/components/Collection/Collection.js
+++ src/pages/home/components/Collection/Collection.js
@@ -7,7 +7,7 @@ import { buttonVariants } from '../../../../constants/constants';
 import { Image } from '../../../../components/Image/Image';
 
 export const Collection = ({
-  items, onCartAdd, cartData, onImageLoaded, onImageError,
+  items, onCartAdd, cartData, onImageError,
 }) => {
   const { t } = useTranslation();
   return (
@@ -25,7 +25,7 @@ export const Collection = ({
             }) => (
               <div className="collectionProductCard" key={key}>
                 <div className="collectionImageContainer">
-                  <Image className="collectionImage" src={image} alt="imageCard" onImageLoaded={onImageLoaded} onError={onImageError} />
+                  <Image className="collectionImage" src={image} alt="imageCard" onError={onImageError} />
                 </div>
                 <p className="collectionTitle">{title}</p>
                 <p className="collectionPrice">{`$ ${price},00 USD`}</p>
@@ -69,7 +69,6 @@ Collection.propTypes = {
     title: PropTypes.string,
     price: PropTypes.number,
   })),
-  onImageLoaded: PropTypes.func.isRequired,
   onImageError: PropTypes.func.isRequired,
 };
 
